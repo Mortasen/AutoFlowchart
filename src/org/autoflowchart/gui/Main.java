43755c0c78853522783d58814bb55a68184a910a
Daryna <kostukdarina44@gmail.com>
2021-04-03 20:23:42 +0300

    First commit

--- src/org/autoflowchart/gui/Main.java
+++ src/org/autoflowchart/gui/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application
     {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 900, 700));
         primaryStage.show();
     }
 
