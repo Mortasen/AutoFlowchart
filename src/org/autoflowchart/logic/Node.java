8562015ee238f75b66ca4975815418938c10ee67
Dalisaca <d.bulanaya@gmail.com>
2021-04-03 20:36:44 +0300

    ///

--- src/org/autoflowchart/logic/Node.java
+++ src/org/autoflowchart/logic/Node.java
@@ -3,7 +3,7 @@ package org.autoflowchart.logic;
 public class Node
 {
 	public Node text;
-	public Node text123;
+	public Node text12345;
 	public boolean type;
 	public Node next;
 	public Node nextFalse;
