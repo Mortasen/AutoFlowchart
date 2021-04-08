71e04e08d139331d2a7d72d96ee4f62c23c03d72
Namerif <namerif7@gmail.com>
2021-04-08 22:31:45 +0300

    Fix import errors and split classes to files

---
+++ src/org/autoflowchart/logic/Nodes.java
@@ -0,0 +1,26 @@
+package org.autoflowchart.logic;
+
+import java.util.ArrayList;
+
+class Nodes extends Node
+{
+	public ArrayList<Node> nodes = new ArrayList<Node>();
+
+	public void add (Node node)
+	{
+		if (node == null)
+			node = null;
+		this.nodes.add(node);
+	}
+
+	public Node getNext ()
+	{
+		return this.nodes.get(0).getNext();
+	}
+
+	public void setNext (Node next)
+	{
+		for (Node node : this.nodes)
+			node.setNext(next);
+	}
+}
