bbd4a96bf8158591d892abff42aa462c32638723
Namerif <namerif7@gmail.com>
2021-05-14 20:37:42 +0300

    Add program to test Designer and Saver

--- src/test/java/org/autoflowchart/logic/DesignerTest.java
+++ src/test/java/org/autoflowchart/logic/DesignerTest.java
@@ -48,7 +48,7 @@ class DesignerTest
 		Layout testLayout = designer.generateLayout(blocks[0]);
 		Saver saver = new Saver();
 		try {
-			saver.save(testLayout, "src/test/resources/img.png");
+			saver.save(testLayout, "src/test/resources/img1.png");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -152,6 +152,80 @@ class DesignerTest
 		blocks[3].nextFalse = block4;
 		Block block5 = new Block(Designer.defaultHeight, ShapeType.RECT, "return;", 0);
 		block4.next = block5;*/
+
+		/*
+		int x = 0;
+		for(int i = 0; i < 1000; i++)
+		{
+			if (i % 2 == 0)
+				continue;
+
+			if (i < 10)
+				int m = 3;
+			else if (i < 100)
+				int m = 2;
+			else
+				int m = 1;
+
+			x += i * m;
+			if (x > 100) {
+				if (i < 100)
+					continue;
+				else
+					break;
+			}
+			x -= 10;
+		}
+		print(x);
+		 */
+
+		Block expected1 = new Block(Designer.defaultHeight, ShapeType.ROUNDRECT, "main();", 0);
+		Block expected2 = new Block(Designer.defaultHeight, ShapeType.RECT, "int x = 0;", 0);
+		expected1.next = expected2;
+		Block expected3 = new Block(Designer.defaultHeight, ShapeType.RECT, "int i = 0;", 0);
+		expected2.next = expected3;
+		Block expected4 = new Block(Designer.defaultHeight, ShapeType.DIAMOND, "i < 1000", 0);
+		expected3.next = expected4;
+		Block expected5 = new Block(Designer.defaultHeight, ShapeType.DIAMOND, "i % 2 == 0", 1);
+		expected4.next = expected5;
+		Block expected6 = new Block(Designer.defaultHeight, ShapeType.DIAMOND, "i < 10", 1);
+		expected5.nextFalse = expected6;
+		Block expected7 = new Block(Designer.defaultHeight, ShapeType.RECT, "int m = 3;", 2);
+		expected6.next = expected7;
+		Block expected8 = new Block(Designer.defaultHeight, ShapeType.DIAMOND, "i < 100", 1);
+		expected6.nextFalse = expected8;
+		Block expected9 = new Block(Designer.defaultHeight, ShapeType.RECT, "int m = 2;", 2);
+		expected8.next = expected9;
+		Block expected10 = new Block(Designer.defaultHeight, ShapeType.RECT, "int m = 1;", 1);
+		expected8.nextFalse = expected10;
+		Block expected11 = new Block(Designer.defaultHeight, ShapeType.RECT, "x += i * m;", 1);
+		expected7.next = expected11;
+		expected9.next = expected11;
+		expected10.next = expected11;
+		Block expected12 = new Block(Designer.defaultHeight, ShapeType.DIAMOND, "x > 100", 1);
+		expected11.next = expected12;
+		Block expected13 = new Block(Designer.defaultHeight, ShapeType.DIAMOND, "i < 100", 2);
+		expected12.next = expected13;
+		Block expected14 = new Block(Designer.defaultHeight, ShapeType.RECT, "x -= 10", 1);
+		expected12.nextFalse = expected14;
+		Block expected15 = new Block(Designer.defaultHeight, ShapeType.RECT, "i++;", 1);
+		expected5.next = expected15;
+		expected13.next = expected15;
+		expected14.next = expected15;
+		expected15.next = expected4;
+		Block expected16 = new Block(Designer.defaultHeight, ShapeType.RECT, "print(x);", 0);
+		expected4.nextFalse = expected16;
+		expected13.nextFalse = expected16;
+		Block expected17 = new Block(Designer.defaultHeight, ShapeType.ROUNDRECT, "return;", 0);
+		expected16.next = expected17;
+
+		designer = new Designer();
+		layout = designer.generateLayout(expected1);
+		try {
+			saver.save(layout, "src/test/resources/img2.png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
