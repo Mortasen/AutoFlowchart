dc2a3dedd756685a59eea02d1ca07d7e991ab4fd
Namerif <namerif7@gmail.com>
2021-05-15 13:51:17 +0300

    Add method that reads file from given path to Processor

--- src/main/java/org/autoflowchart/logic/Processor.java
+++ src/main/java/org/autoflowchart/logic/Processor.java
@@ -4,6 +4,8 @@ import org.autoflowchart.objects.Block;
 import org.autoflowchart.objects.Layout;
 import org.autoflowchart.objects.Node;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Processor
@@ -15,5 +17,11 @@ public class Processor
 		Block firstBlock = labeler.labelNodes(firstNode);
 		Layout layout = designer.generateLayout(firstBlock);
 		return layout;
+	}
+
+	public Layout process (Path filepath) throws IOException
+	{
+		String code = Files.readString(filepath);
+		return this.process(code);
 	}
 }
