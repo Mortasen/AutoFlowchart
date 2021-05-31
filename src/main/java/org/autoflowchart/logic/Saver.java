03c6bcfb529429bb18bedf1a9b25fc5545af4d1c
Anna <ann.ponochovna@gmail.com>
2021-05-31 23:04:42 +0300

    Базовые функции: сохранить в пнг, отрисовать учитывая кастомайзер(без размера), создала конструктор для кастомайзера(тестовый), его переделать нужно будет

--- src/main/java/org/autoflowchart/logic/Saver.java
+++ src/main/java/org/autoflowchart/logic/Saver.java
@@ -1,10 +1,7 @@
 package org.autoflowchart.logic;
-
 import org.autoflowchart.objects.*;
 import org.autoflowchart.objects.Shape;
-
 import javax.imageio.ImageIO;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,10 +9,10 @@ import java.io.IOException;
 
 public class Saver
 {
-	public void save (Layout layout, String filepath) throws IOException
+	BufferedImage img = new BufferedImage(1200, 2000, BufferedImage.TYPE_INT_RGB);
+	Graphics2D g = img.createGraphics();
+	public void save (Layout layout, String filepath, CustomizerOptions custom) throws IOException
 	{
-		BufferedImage img = new BufferedImage(1000, 2000, BufferedImage.TYPE_INT_RGB);
-		Graphics2D g = img.createGraphics();
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, img.getWidth(), img.getHeight());
 		g.setColor(Color.BLACK);
@@ -23,25 +20,68 @@ public class Saver
 		g.setFont(font);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		for (Shape shape : layout.shapes)
+
+		float outline = custom.OutlineSize;
+		BasicStroke Stroke = new BasicStroke(2.0f*outline);
+		g.setStroke(Stroke);
+		int j=0;
+		for ( Shape shape : layout.shapes)
 		{
+
+			Arrow arrow = layout.arrows.get(j);
 			int x = shape.x / 2;
 			int y = shape.y / 2;
 			int width = shape.width / 2;
+
 			int height = shape.height / 2;
 			if (shape.type == ShapeType.ROUNDRECT)
 			{
-				g.drawRoundRect(x, y, width, height, height / 2, height / 2);
-			} else if (shape.type == ShapeType.RECT) {
-				g.drawRect(x, y, width, height);
-			} else if (shape.type == ShapeType.DIAMOND) {
-				int[] xPoints = { x, x + width / 2, x + width, x + width / 2 };
-				int[] yPoints = { y + height / 2, y, y + height / 2, y + height };
-				g.drawPolygon(xPoints, yPoints, 4);
-			}
-			g.drawString(shape.text, x, y + 20);
-		}
+				DrawObject(shape, custom.terminalShape, custom.terminalColor, custom.terminalOutlineColor, custom.terminalSize);
 
+//				int xx = (int) (arrow.xPoints.get(j) * custom.terminalSize);
+//				int yy = (int) (arrow.yPoints.get(j) * custom.terminalSize);
+//				arrow.xPoints.set(j, xx);
+//				arrow.yPoints.set(j, yy);
+//				//xx = (int) (arrow.xPoints.get(j+1) * custom.terminalSize);
+//				//xx = (int) (arrow.xPoints.get(j) * custom.terminalSize);
+//			//	yy += (layout.arrows.yPoints.get(j+1) - arrow.yPoints.get(j));
+//			//	yy = (int) (arrow.yPoints.get(j+1) * custom.terminalSize);
+//				yy = (int) (arrow.yPoints.get(j) * custom.terminalSize) - (int) (yy/ custom.terminalSize);
+//				arrow.xPoints.set(j+1, xx);
+//				arrow.yPoints.set(j+1, yy);
+//				layout.arrows.set(j, arrow);
+//				shape = layout.shapes.get(j+1);
+//				shape.x*=custom.terminalSize;
+//				shape.y*=custom.terminalSize;
+//				layout.shapes.set(j+1, shape);
+//				j++;
+			} else if (shape.type == ShapeType.RECT) {
+				DrawObject(shape, custom.processShape, custom.processColor, custom.processOutlineColor, custom.processSize);
+			} else if (shape.type == ShapeType.DIAMOND) {
+				DrawObject(shape, custom.decisionShape, custom.decisionColor, custom.decisionOutlineColor, custom.decisionSize);
+			}
+			else if (shape.type == ShapeType.PARALLELOGRAM){
+
+				DrawObject(shape, custom.outputShape, custom.outputColor, custom.outputOutlineColor, custom.outputSize);
+			}
+			else if (shape.type==ShapeType.OVAL){
+				DrawObject(shape, custom.terminalShape, custom.terminalColor, custom.terminalOutlineColor, custom.terminalSize);
+			}
+			else if (shape.type==ShapeType.HEXAGON){
+				DrawObject(shape, custom.processShape, custom.processColor, custom.processOutlineColor, custom.processSize);
+			}
+			else if(shape.type==ShapeType.FUNCTION){
+				DrawObject(shape, custom.functionShape, custom.functionColor, custom.functionOutlineColor, custom.functionSize);
+			}
+			FontMetrics fm = img.getGraphics().getFontMetrics(font);
+			int widthStr = fm.stringWidth(shape.text);
+			int w = width;
+			int h = (height)/2;
+			y+=h;
+			x+=Math.abs(w-widthStr)/2;
+			g.drawString(shape.text, x, y);
+		}
+		g.setColor(new Color(57, 11, 11));
 		for (Arrow arrow : layout.arrows)
 		{
 			int[] xPoints = new int[arrow.xPoints.size()];
@@ -57,5 +97,63 @@ public class Saver
 
 		File outputFile = new File(filepath);
 		ImageIO.write(img, "png", outputFile);
+	}
+	public void DrawObject(Shape shape, ShapeType type, Color BlockColor, Color LineColor, double Size) {
+		int x = shape.x / 2;
+		int y = shape.y / 2;
+		int width = shape.width / 2;
+
+		int height = shape.height / 2;
+		//height*=Size;
+		//width*=Size;
+		if (type == ShapeType.ROUNDRECT) {
+			g.setColor(BlockColor);
+			g.fillRoundRect(x, y, width, height, height / 2, height / 2);
+
+			g.setColor(LineColor);
+			g.drawRoundRect(x, y, width, height, height / 2, height / 2);
+		} else if (type == ShapeType.RECT) {
+			g.setColor(BlockColor);
+			g.fillRect(x, y, width, height);
+			g.setColor(LineColor);
+			g.drawRect(x, y, width, height);
+		} else if (type == ShapeType.DIAMOND) {
+			int[] xPoints = {x, x + width / 2, x + width, x + width / 2};
+			int[] yPoints = {y + height / 2, y, y + height / 2, y + height};
+			g.setColor(BlockColor);
+			g.fillPolygon(xPoints, yPoints, 4);
+			g.setColor(LineColor);
+			g.drawPolygon(xPoints, yPoints, 4);
+		} else if (type == ShapeType.PARALLELOGRAM) {
+			int six = width / 6;
+			int[] xPoints = {x, x + six, x + width, x + 5 * six};
+			int[] yPoints = {y + height, y, y, y + height};
+			g.setColor(BlockColor);
+			g.fillPolygon(xPoints, yPoints, 4);
+			g.setColor(LineColor);
+			g.drawPolygon(xPoints, yPoints, 4);
+
+		} else if (type == ShapeType.OVAL) {
+			g.setColor(BlockColor);
+			g.fillOval(x, y, width, height);
+			g.setColor(LineColor);
+			g.drawOval(x, y, width, height);
+		} else if (type == ShapeType.HEXAGON) {
+			int hghtHalf = height / 2;
+			int wdthThird = width / 3;
+			int[] xPoints = {x, x + wdthThird, x + 2 * wdthThird, x + width, x + 2 * wdthThird, x + wdthThird};
+			int[] yPoints = {y + hghtHalf, y, y, y + hghtHalf, y + height, y + height};
+			g.setColor(BlockColor);
+			g.fillPolygon(xPoints, yPoints, 6);
+			g.setColor(LineColor);
+			g.drawPolygon(xPoints, yPoints, 6);
+		} else if (type == ShapeType.FUNCTION) {
+			g.setColor(BlockColor);
+			g.fillRect(x, y, width, height);
+			g.setColor(LineColor);
+			g.drawRect(x, y, width, height);
+			g.drawLine(x + 14, y, x + 14, y + height);
+			g.drawLine(x + width - 14, y, x + width - 14, y + height);
+		}
 	}
 }
